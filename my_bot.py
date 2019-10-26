497cb9e0bb76c8978a73f9d1d459a9c092028958
piedpiperru <57037994+piedpiperru@users.noreply.github.com>
2019-10-26 18:20:05 +0300

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -4,6 +4,6 @@ bot = telebot.TeleBot('904476426 :AAHh7Yrf5uEixLvJnM-GPcE27ndrnyNGZbM')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'sdfsdgdfhfgjfjhgj /start')
 
 bot.polling()
