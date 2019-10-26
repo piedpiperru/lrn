3f4f52d775e3743255204bac46d6bce8c60ed12c
piedpiperru <57037994+piedpiperru@users.noreply.github.com>
2019-10-26 20:08:26 +0300

    Delete my_bot.py

--- my_bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('904476426 :AAHh7Yrf5uEixLvJnM-GPcE27ndrnyNGZbM')
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'sdfsdgdfhfgjfjhgj /start')
-
-bot.polling()
